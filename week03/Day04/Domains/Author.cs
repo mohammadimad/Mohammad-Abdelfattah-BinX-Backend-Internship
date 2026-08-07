598e165b98b21fd89854b079fb904b2a068fb5ae
Mohammad <hammodaimad@gmail.com>
2026-08-07 19:10:11 +0300

    upload solution task day 4

---
+++ week03/Day04/Domains/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace Day03.Domain
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Email { get; set; } 
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
