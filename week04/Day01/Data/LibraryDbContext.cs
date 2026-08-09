0919099a6c508317b9953ec0b8fc19253b819d4c
Mohammad <hammodaimad@gmail.com>
2026-08-09 22:11:36 +0300

    upload solution tasks day one

---
+++ week04/Day01/Data/LibraryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Day03.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day03.Data
+{
+    public class LibraryDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            
+            
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<MemberPhone> MemberPhones { get; set; }
+        public DbSet<LendingRecord> LendingRecords { get; set; }
+    }
+}
